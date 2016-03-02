34bb94b147545d58a9d76932d33f020a31ac71a4
Arturo Fornés Arvayo <fornesarturo@gmail.com>
2016-03-02 12:10:23 -0600

    Update buzzerButton.py

--- buzzerButton.py
+++ buzzerButton.py
@@ -1,7 +1,12 @@
+import random
 import time
 import pyupm_buzzer as upmBuzzer
 import pyupm_grove as grove
+import pyupm_i2clcd as lcd
 
+# Initialize Jhd1313m1 at 0x3E (LCD_ADDRESS) and 0x62 (RGB_ADDRESS)
+myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
+myLcd.setCursor(0,0)
 # Create the button object using GPIO pin 3
 button = grove.GroveButton(3)
 
@@ -12,12 +17,15 @@ chords = [upmBuzzer.DO, upmBuzzer.RE, upmBuzzer.MI, upmBuzzer.FA,
           upmBuzzer.SOL, upmBuzzer.LA, upmBuzzer.SI, upmBuzzer.DO, 
           upmBuzzer.SI];
 
-# Make a DO sound if button value is True.
+# Make a DO sound if button value is True and change myLcd color to a random value.
 while 1:
-    if button.value():
-          print buzzer.playSound(chords[0], 1000000)
-    #time.sleep(1)
-
+	myLcd.setCursor(0,0)
+	contador = 0
+    	if button.value():
+	          myLcd.setColor(random.randint(0,255),random.randint(0,255),random.randint(0,255))
+	          print buzzer.playSound(chords[0], 1000000)
+	          contador += 1
+    		
 # Delete the buzzer object
 del buzzer
 
